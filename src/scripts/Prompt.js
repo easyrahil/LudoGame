95aae544c003112189bfa904f69aac6d00d5d159
AbhishekRana13 <abhishek@codecrust.in>
2021-09-10 14:01:40 +0530

    Updated Prompt Class

--- src/scripts/Prompt.js
+++ src/scripts/Prompt.js
@@ -1,16 +1,32 @@
 import * as PIXI from "pixi.js";
+import { gameConfig } from "./appConfig";
 import { Globals } from "./Globals";
-
+import TWEEN from "@tweenjs/tween.js";
 
 export class Prompt
 {
-    constructor()
+    constructor(text, position, size = "44", color = "#fff")
     {
         this.container = new PIXI.Container();
         
 
         this.createBackground();
-        this.createForegroundText();
+        this.createForegroundText(text, size, color);
+
+        this.container.scale.set(gameConfig.widthRatio);
+        this.finalPosition = new PIXI.Point(position);
+        this.container.position = new PIXI.Point(this.finalPosition.x - this.container.width, this.finalPosition.y);
+
+        const fallBackTween = new TWEEN.Tween(this.container)
+                            .to({x : this.finalPosition.x - this.container.width}, 100)
+                            .onComplete()
+                            .start();
+            
+
+        const tween = new TWEEN.Tween(this.container)
+                    .to({x : this.finalPosition.x}, 100)
+                    .onComplete()
+                    .start();
     }
 
 
@@ -20,12 +36,12 @@ export class Prompt
         this.background.anchor.set(0, 0.5);
     }
 
-    createForegroundText(text)
+    createForegroundText(text, size, color)
     {
         this.text = new PIXI.Text(text,{
 
         });
-
+        
         this.text.style = {
             fontFamily: "Verdana",
             fontWeight: "bold",
@@ -33,5 +49,6 @@ export class Prompt
             fill: [color]
         };
         this.text = text;
+        this.text.anchor.set(0.5);
     }
 }
