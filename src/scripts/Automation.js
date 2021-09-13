b3f38cb6c3e08de29338665b3e3817e251f70673
AbhiRana13 <abhirana13@outlook.com>
2021-09-13 15:30:35 +0530

    Added Automation Class

---
+++ src/scripts/Automation.js
@@ -0,0 +1,33 @@
+import { Globals } from "./Globals";
+
+
+export class Automation
+{
+    constructor(playerRef)
+    {
+        this.player = playerRef;
+    }
+
+    RollDice(gameSceneRef)
+    {
+        if(gameSceneRef.players[Globals.gameData.plId].hasAutomation)
+		{
+			const distmsg = {
+				t: "pDiceRoll"
+			}
+			Globals.socket.sendMessage(distmsg);
+			Globals.resources.click.sound.play();
+			//Send Message to server
+			gameSceneRef.playDiceAnimation();
+		}
+    }
+
+    selectPawn()
+    {
+        this.player.pawnSelected(this.player.pawnsID[Math.ceil(Math.random() * this.player.pawnsID.length) - 1]);
+    }
+
+
+
+
+}
